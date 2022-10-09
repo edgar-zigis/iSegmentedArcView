bd9cc9c66b6f932e8fff774f4f28938fb6df8191
Edgar Žigis <tranced.freak@gmail.com>
2022-10-09 12:37:58 +0300

    Add basic objects (#2)

---
+++ Sources/iSegmentedArcView/Custom/ArcSegment.swift
@@ -0,0 +1,17 @@
+//
+//  ArcSegment.swift
+//  iSegmentedArcView
+//
+//  Created by Edgar Žigis on 2022-10-09.
+//  Copyright © 2022 Edgar Žigis. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct ArcSegment {
+    let gradientStart: UIColor
+    let gradientEnd: UIColor
+    let animate: Bool = false
+    let sweepAngle: Double = 0
+}
---
+++ Sources/iSegmentedArcView/Custom/BlinkAnimationSettings.swift
@@ -0,0 +1,16 @@
+//
+//  BlinkAnimationSettings.swift
+//  iSegmentedArcView
+//
+//  Created by Edgar Žigis on 2022-10-09.
+//  Copyright © 2022 Edgar Žigis. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct BlinkAnimationSettings {
+    let minAlpha: CGFloat
+    let maxAlpha: CGFloat
+    let duration: TimeInterval
+}
